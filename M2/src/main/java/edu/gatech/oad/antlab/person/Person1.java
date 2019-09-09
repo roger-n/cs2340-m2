1e537d4b4da2a361c6c309b8f288028a33f562a4
Roger Nhan <rn55125@gmail.com>
2019-09-09 13:28:59 -0400

    Person1 file (Roger Nhan)

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Roger Nhan
  *  @version 1.1
  */
 public class Person1 {
@@ -30,8 +30,8 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  int len = input.length();
+	  return input.substring(len - 2, len) + input.substring(0, len - 2);
 	}
 	
 	/**
